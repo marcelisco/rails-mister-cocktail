4ecfdc7abe01e668323ada754087a827a9789ff0
Marcelo Oliveira <mfoliveira88@gmail.com>
2021-02-18 18:25:30 -0300

    new e create feitos

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,13 +5,27 @@ class CocktailsController < ApplicationController
   end
 
   def show
-    #detalhes de um cocktail, preciso achar o cocktail que eu quero mostrar
+    # detalhes de um cocktail, preciso achar o cocktail que eu quero mostrar
     @cocktail = Cocktail.find(params[:id])
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def new
+    # página formulário para criar um novo cocktail
+    @cocktail = Cocktail.new
   end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
 end
